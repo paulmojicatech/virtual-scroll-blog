1c9a4e696badcc29561ba7dabc6caf9aa88b9693
Paul Mojica <paulmojicatech@gmail.com>
2024-09-17 10:05:48 -0400

    add deferred view

--- src/app/deferred-view/deferred-view.component.ts
+++ src/app/deferred-view/deferred-view.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { GithubCardComponent } from '../components/github-card/github-card.component';
+import { GithubHttpService } from '../services/github-http.service';
+import { GithubRepoHttpResponse } from '../models/repos.interface';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-deferred-view',
   standalone: true,
-  imports: [],
-  templateUrl: './deferred-view.component.html',
-  styleUrl: './deferred-view.component.scss'
+  imports: [GithubCardComponent],
+  template:`
+    <div class="container">
+      @for (repo of reposS(); track $index) {
+        @defer(on viewport) {
+          <div class="card-container">
+            <app-github-card [repoS]="repo"></app-github-card>
+          </div>
+        } @placeholder {
+          <span>Loading...</span>
+        }
+      }
+    </div>
+  `,
+  styles: `
+  .container {
+    height: calc(100vh - 6rem);
+  }
+  .card-container {
+      margin: 1rem;      
+    }
+  `
 })
-export class DeferredViewComponent {
+export class DeferredViewComponent implements OnInit {
+  private _githubSvc = inject(GithubHttpService);
+  reposS = signal<GithubRepoHttpResponse[]>([]);
 
+  ngOnInit(): void {
+    this._githubSvc.getGithubRepos().pipe(take(1)).subscribe(repos => {
+      this.reposS.set(repos);
+    });
+  }
 }
